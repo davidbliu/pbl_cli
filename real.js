f7599d977868f3582acdaed66237b498c0e72565
David Liu <davidbliu@gmail.com>
2015-12-30 22:58:32 -0800

    ui bookmark

--- real.js
+++ real.js
@@ -4,12 +4,44 @@ var clientId = '158145275272-lj3m0j741dj9fp50rticq48vtrfu59jj.apps.googleusercon
 var realtimeUtils = new utils.RealtimeUtils({ clientId: clientId });
 var myDoc;
 var myAuth;
-
 var tabMap;
+var bookmarks;
 app.controller('CopilotCtrl', function($scope){
   $scope.msg = 'hi there';
   $scope.tabDict = {};
   $scope.collaborators = [];
+
+  $scope.bookmarkTab = function(tab){
+    seen = false;
+    _.each(_.range(bookmarks.length), function(i){
+      if(bookmarks.get(i).url == tab.url){
+        seen = true;
+      }
+    });
+    if(!seen){
+      bookmarks.push(tab); 
+    }
+  }
+  $scope.removeBookmark = function(tab){
+    //remove all bookmarks with this tabs url
+    console.log('removing bookmark');
+    marks = _.map(_.range(bookmarks.length), function(i){
+      return bookmarks.get(i);
+    });
+    marks = _.filter(marks, function(x){
+      return x.url != tab.url;
+    });
+    bookmarks.removeRange(0,bookmarks.length);
+    _.each(marks, function(mark){
+      bookmarks.push(mark);
+    });
+  }
+
+  $scope.activeClass = function(tab){
+    if(tab.active){
+      return 'active-tab';
+    }
+  }
   authorize();
 
   function authorize() {
@@ -47,13 +79,21 @@ app.controller('CopilotCtrl', function($scope){
   function onFileLoaded(doc) {
     myDoc = doc;
     keys = myDoc.getModel().getRoot().keys();
-    tabMap = myDoc.getModel().getRoot().get('mainChannel');
+    //get tabMap for currently open tabs
+    tabMap = myDoc.getModel().getRoot().get('main');
     tabMap.addEventListener( gapi.drive.realtime.EventType.VALUE_CHANGED, function(event){
       translateTabMap(tabMap);
     });
+    //get bookmarks
+    bookmarks = myDoc.getModel().getRoot().get('main-bookmarks');
+    bookmarks.addEventListener(gapi.drive.realtime.EventType.VALUES_ADDED, function(event){
+      translateBookmarks(bookmarks);
+    });
+    translateBookmarks(bookmarks);
     activateMessages();
     handleCollaborators();
   }
+
   
   function updateCollaborators(){
     $scope.collaborators = myDoc.getCollaborators();
@@ -72,6 +112,16 @@ app.controller('CopilotCtrl', function($scope){
     });
   }
 
+  function translateBookmarks(bookmarks){
+    $scope.bookmarks = _.map(_.range(bookmarks.length), function(i){
+      return bookmarks.get(i);
+    });
+    setTimeout(function(){
+      activateTabHover();
+      console.log('activated');
+    }, 1000);
+  }
+
   function translateTabMap(tabMap){
     var userids = _.keys($scope.userDict);
     $scope.tabDict = {};
@@ -82,6 +132,7 @@ app.controller('CopilotCtrl', function($scope){
     });
     $scope.userIds = _.keys($scope.tabDict);
     $scope.$digest();
+    activateTabHover();
   }
 
   //receive messages from chrome extension
@@ -97,3 +148,12 @@ app.controller('CopilotCtrl', function($scope){
   }
 
 });//end of a super long controller
+
+function activateTabHover(){
+  $('.tab-li').unbind('mouseenter mouseleave');
+  $('.tab-li').hover(function(){
+    $(this).find('.tab-icons').show();
+  }, function(){
+    $(this).find('.tab-icons').hide();
+  });
+}
